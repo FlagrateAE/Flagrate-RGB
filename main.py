b279f9f7eaca7eff974e2c4c249911a6f5ff4067
FlagrateAE <flagrateinc@gmail.com>
2024-10-09 20:25:50 +0300

    Release
    Stable version with structurised and improved main code

--- main.py
+++ main.py
@@ -9,59 +9,72 @@ from colorist import rgb
 import yaml
 
 
-def RUN():
-    arduino = Arduino("COM3")
+def RUN(_logging=False):
+    """Main function
 
+    Parameters
+    ----------
+    _logging : bool
+        Whether to print advanced debug messages (default: False)
+    """
+
+    # initialize everything
+    arduino = Arduino("COM3")
     sp = SpotifyColorExtractor(
         clientID=os.getenv("CLIENT_ID"),
         clientSecret=os.getenv("CLIENT_SECRET"),
         redirectURI="http://localhost:50000/",
     )
-
     tray = Tray()
 
-    lastAlbumID = None
+    lastAlbumID = True
     while tray.fRunning:
-        playback = sp.getCurrentPlayback()
+        # get current playing track from Spotify
+        playback: Playback = sp.getCurrentPlayback()
 
         if playback:
+            # check if album has changed
             currentAlbumID = playback.albumID
-
             if currentAlbumID != lastAlbumID:
-                print(yaml.dump(playback.__dict__))
 
-                mainColor = sp.extractMainColor(playback=playback, _logging=True)
-                rgb(
-                    f"\nBest color match: {mainColor}",
-                    mainColor[0],
-                    mainColor[1],
-                    mainColor[2],
-                )
+                if _logging:
+                    print(yaml.dump(playback.__dict__))
+                else:
+                    print(
+                        f'Now playing: {playback.artist} - {playback.track} from "{playback.albumName}"'
+                    )
 
+                # get main color
+                mainColor = sp.extractMainColor(playback=playback, _logging=_logging)
                 lastAlbumID = currentAlbumID
 
-                if mainColor == (255, 255, 255):
-                    print("COMMAND = 12.")
-
-                    ardColor = mainColor
-                    arduino.send("12.")
-                else:
-                    command, ardColor = getNearestColorCode(mainColor, _logging=True)
-
+                if _logging:
                     rgb(
-                        f"MAIN COLOR: {ardColor}", ardColor[0], ardColor[1], ardColor[2]
+                        f"\nBest color from image: {mainColor}",
+                        mainColor[0],
+                        mainColor[1],
+                        mainColor[2],
                     )
-                    print(f"COMMAND = {command}.\n")
 
-                    arduino.send(f"{command}.")
+                if mainColor == (255, 255, 255):
+                    LEDColor = (255, 255, 255)
+                    command = 12
+                else:
+                    # get my color code for RGB LED
+                    command, LEDColor = getNearestColorCode(mainColor, _logging=True)
 
-                tray.spotify(playback=playback, color=ardColor)
+                # execute color transmission and display in console and tray
+                print(f"COMMAND = {command}.\n")
+                arduino.send(f"{command}.")
+                rgb(f"COLOR SENT: {LEDColor}", LEDColor[0], LEDColor[1], LEDColor[2])
+                tray.spotify(playback=playback, color=LEDColor)
         else:
-            lastAlbumID = None
-            print("No playback detected")
+            if lastAlbumID:
+                print("No playback detected")
+                lastAlbumID = None
 
         time.sleep(2)
 
 
 if __name__ == "__main__":
-    RUN()
+    RUN(_logging=True)
